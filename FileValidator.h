a48f15e7635a524943877060f7737ea31c88f8da
Nora Hollerith <norahollerith@gmail.com>
2022-11-01 15:13:03 +0100

    Updated Regex after test case failure

--- FileValidator.h
+++ FileValidator.h
@@ -14,7 +14,7 @@
 static bool isValidLz4(const std::string& fileName){
     //std::cout << "Checking File " << fileName << std::endl;
 
-    std::regex re(R"(^core\..*\..*\.lz4$)");
+    std::regex re(R"(^core\.[a-zA-Z]+\..*\.lz4$)");
     std::smatch matches;
     auto const match = std::regex_search(fileName, re);
     //std::cout << "Is there a match: " << match << std::endl;
